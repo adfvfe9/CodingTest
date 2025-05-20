e7148c2c64125869215fcd3f87c2f7e1d9e792eb
공은성 <137687927+adfvfe9@users.noreply.github.com>
2025-05-20 14:43:08 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -6,9 +6,9 @@ class Solution {
         }
         int[] answer = new int[cnt];
         cnt = 0;
-        for (int i = 0; i < numlist.length; i++) {
-            if (numlist[i] % n == 0) {
-                answer[cnt] = numlist[i];
+        for (int i : numlist) {
+            if (i % n == 0) {
+                answer[cnt] = i;
                 cnt++;
             }
         }
