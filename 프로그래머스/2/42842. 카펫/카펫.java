99629fddb35bcbd457c348ee3c94ed246e700e30
공은성 <137687927+adfvfe9@users.noreply.github.com>
2026-01-31 20:50:44 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -4,7 +4,7 @@ class Solution {
         for (int i = 3; i <= allTile / 3; i++) {
             if (allTile % i == 0) {
                 if (brown == allTile / i * 2 + (i - 2) * 2) {
-                    return new int[]{Math.max(allTile / i, i), Math.min(allTile / i, i)};
+                    return new int[]{allTile / i, i};
                 }
             }
         }
