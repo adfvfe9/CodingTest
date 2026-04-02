c47b3ff73a67b0bff45334f0fec380cfb6512917
공은성 <137687927+adfvfe9@users.noreply.github.com>
2026-04-03 08:05:45 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,14 +1,21 @@
 class Solution {
     public int solution(int n, int a, int b) {
-        int answer = 1;
-        while (true) {
-            if (Math.abs(a - b) == 1) {
-                if ((a > b && a % 2 == 0) || (b > a && b % 2 == 0)) {
-                    break;
-                }
-            }
-            a = (int)Math.ceil((double)a / 2);
-            b = (int)Math.ceil((double)b / 2);
+        // int answer = 1;
+        // while (true) {
+        //     if (Math.abs(a - b) == 1) {
+        //         if ((a > b && a % 2 == 0) || (b > a && b % 2 == 0)) {
+        //             break;
+        //         }
+        //     }
+        //     a = (int)Math.ceil((double)a / 2);
+        //     b = (int)Math.ceil((double)b / 2);
+        //     answer++;
+        // }
+        // return answer;
+        int answer = 0;
+        while (a != b) {
+            a = a / 2 + (a % 2);
+            b = b / 2 + (b % 2);
             answer++;
         }
         return answer;
